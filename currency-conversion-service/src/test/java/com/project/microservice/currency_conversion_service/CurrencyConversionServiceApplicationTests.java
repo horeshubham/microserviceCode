bd3c5a6d021b9ade57637d5d15a060f75ed612a8
Shubham Hore <shore@innominds.com>
2024-07-08 11:02:55 +0530

    2nd commit

---
+++ currency-conversion-service/src/test/java/com/project/microservice/currency_conversion_service/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservice.currency_conversion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ currency-exchange-service/src/test/java/com/project/microservice/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservice.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
